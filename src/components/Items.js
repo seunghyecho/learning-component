ad4a7f194417b0fb426defd36ba596e3d75119d7
seunghyecho <53107641+seunghyecho@users.noreply.github.com>
2023-08-13 17:30:41 +0900

    이벤트 처리, 이벤트 버블링

--- src/components/Items.js
+++ src/components/Items.js
@@ -19,22 +19,22 @@ export default class Items extends Component {
               .join("")}
         
         </ul>
-        <button id="append">추가</button>
+        <button class="addBtn">추가</button>
         `;
   }
 
   setEvent() {
-    this.$target.querySelector("button").addEventListener("click", () => {
-      const { items } = this.state;
-      this.setState({ items: [...items, `item${items.length + 1}`] });
-    });
+    this.$target.addEventListener("click", ({ target }) => {
+      const items = [...this.state.items];
 
-    this.$target.querySelectorAll(".deleteBtn").forEach((deleBtn) =>
-      deleBtn.addEventListener("click", ({ target }) => {
-        const items = [...this.state.items];
+      if (target.classList.contains("addBtn")) {
+        this.setState({ items: [...items, `item${items.length + 1}`] });
+      }
+
+      if (target.classList.contains("deleteBtn")) {
         items.splice(target.dataset.index, 1);
         this.setState({ items });
-      })
-    );
+      }
+    });
   }
 }
